f09a502faeb78c1c2834488ef61e199ff762d3ed
biyrololo <92595932+biyrololo@users.noreply.github.com>
2024-04-21 21:58:07 +0400

    fix time

--- client/src/pages/TestPage.tsx
+++ client/src/pages/TestPage.tsx
@@ -17,7 +17,7 @@ const ActionsButton = styled(Button)({
 })
 
 function getDate(){
-    const date = new Date(new Date().getTime() + 60*60*60);
+    const date = new Date(new Date().getTime() + 60*60*1000*4);
     return date.toISOString().replace(/T/, ' ').replace(/\..+/, '')
 }
 
